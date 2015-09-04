3cb8ce7b3f5862ac16592ff01e089f0b57f4514c
David Pershouse <dpershouse@gmail.com>
2015-09-04 17:07:21 +1000

    optimise carfront don't mangle colors

--- build.bat
+++ build.bat
@@ -5,6 +5,6 @@ call cleancss -o dist\style.css style.css
 
  call html-minifier --collapse-whitespace --remove-optional-tags --remove-script-type-attributes --remove-style-link-type-attributes   index.html --output dist\index.html
 call uglifyjs -mt -b beautify=false,ascii-only=true  -o dist\reversed.js musicplayer.js music.js reversed.js
-call svgo -f a -o dist\a
+call svgo -f a --disable=convertColors -p 2  -o dist\a
 "c:\Program Files\7-Zip\7z.exe" a -mx9 -r -tzip reversed.zip dist\*.*
 dir reversed.zip
